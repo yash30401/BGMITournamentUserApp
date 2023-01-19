8096920e6c2fb229de5891e46703b54edca46446
yash30401 <yashveersingh30401@gmail.com>
2023-01-19 17:36:45 +0530

    1. Drawer Design Implemented

--- app/src/main/java/com/bgmi/sports/tournament/userApp/bgmitournament/MainActivity.kt
+++ app/src/main/java/com/bgmi/sports/tournament/userApp/bgmitournament/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.bgmi.sports.tournament.userApp.bgmitournament
 
 import android.content.Intent
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bgmi.sports.tournament.userApp.bgmitournament.Auth.RegisterUser
+import com.bgmi.sports.tournament.userApp.bgmitournament.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 
@@ -11,13 +13,21 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var firebaseUser: FirebaseUser
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         supportActionBar?.hide()
+
+
 
         firebaseAuth=FirebaseAuth.getInstance()
         firebaseUser=firebaseAuth.currentUser!!
+
+
     }
 
     override fun onStart() {
